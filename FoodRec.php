724fa278ee7f7231476c18d4fb0674aff4525e5b
wachirawit autamakorn <wachirawitautamakorn@gmail.com>
2025-07-12 03:09:37 +0700

    wow

---
+++ FoodRec.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RecMenu</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body class="body"> 
+<?php include "./components/navbar.php"?>
+
+<div><a href="./index.php"><img src="./assets/img/back-arrow.png" alt="" style="width: 50px; margin-top: 5px; margin-left: 15px; margin-bottom: 15px;"></a></div>
+
+<div class="container text-center nav-monk">
+  <div class="row">
+    <div class="col" style="color: #d3d3d3;">
+        <a href="" class="font-nav">< อาหารหลัก</a>
+    </div>
+    <div class="col">
+      เมนูแนะนำ
+    </div>
+    <div class="col" style="color: #d3d3d3; ">
+      <a href="" class="font-nav">อาหารรองฮี้ย ></a>
+    </div>
+  </div>
+</div>
+<div class="container mt-4">
+  <div class="row justify-content-center g-5 " >
+
+    <!-- การ์ด 1 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px;">
+        <img src="https://www.wendys.com/sites/default/files/styles/max_650x650/public/2021-05/daves-double.png?itok=0LISzLWe" class="card-img-top mt-3" style="width: 200px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">Double Cheese Burger</p>
+          <p class="food-font1">Price 35 ฿</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- การ์ด 2 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px; ">
+        <img src="https://static.vecteezy.com/system/resources/previews/025/062/054/non_2x/french-fries-on-white-bowl-isolated-on-transparent-background-fried-fast-food-snack-isolated-food-photography-junk-food-fried-potatoes-free-png.png" class="card-img-top mt-3" style="width: 200px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">French Fires</p>
+          <p class="food-font1">Price 35 ฿</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- การ์ด 3 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px; ">
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/023/450/138/small_2x/side-view-of-hot-latte-coffee-with-heart-latte-art-created-with-generative-ai-png.png" class="card-img-top mt-3" style="width: 220px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">Latte</p>
+          <p class="food-font1">Price 35 ฿</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- การ์ด 4 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px; ">
+        <img src="https://assets.noshu.com/uploads/2022/03/Noshu-BrownieMix-Hero-700px.png" class="card-img-top mt-3" style="width: 200px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">Brownnie</p>
+          <p class="food-font1">Price 35 ฿</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container mt-4">
+  <div class="row justify-content-center g-5 " >
+
+    <!-- การ์ด 1 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px;">
+        <img src="https://png.pngtree.com/png-vector/20240624/ourmid/pngtree-fried-rice-with-basil-minced-pork-and-egg-thai-food-transparent-png-image_12841981.png" class="card-img-top mt-3" style="width: 200px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">Pad Kra Pao (หมู)</p>
+          <p class="food-font1">Price 35 ฿</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- การ์ด 2 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px; ">
+        <img src="https://img.wongnai.com/p/256x256/2025/01/18/970d6273045c4b9db8cb9b669514c06b.jpg" class="card-img-top mt-3" style="width: 200px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">Omlette with Rice</p>
+          <p class="food-font1">Price 35 ฿</p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+
+    <!-- การ์ด 3 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px; ">
+        <img src="https://www.7-eleven.ca/_next/image?url=https%3A%2F%2Fimages.contentstack.io%2Fv3%2Fassets%2Fbltbb619fd5c667ba2d%2Fblt0bca16d59fb153f3%2F67ad604170ce63843238ab5c%2FPortuguese-Egg-Tart-Product-Card.png&w=3840&q=75" class="card-img-top mt-3" style="width: 200px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">Egg Tart</p>
+          <p class="food-font1">Price 35 ฿</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- การ์ด 4 -->
+    <div class="col-6 col-md-3 d-flex justify-content-center">
+      <div class="card align-items-center menu-pic border-0" style="width: 17rem; border-radius: 25px; ">
+        <img src="" class="card-img-top mt-3" style="width: 200px;" height="200px" alt="...">
+        <div class="card-body">
+          <p class="food-font">double chaese burger</p>
+          <p class="food-font1">Price 35 ฿</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+<footer><img src="./assets/img/cart-fix.png" class="floating-circle" alt="Circle Image"></footer>
+</html>
--- components/navbar.php
+++ components/navbar.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg nav-color">
   <div class="container-fluid">
   <div>
-<img src="./imgg/152431942_114763933966355_8265361494354481544_n.png" alt="" width="100px" height="100px">
+<img src="./assets/img/152431942_114763933966355_8265361494354481544_n.png" alt="" width="100px" height="100px">
 </div>
 <div class="ms-auto">
-  <a href="https://www.youtube.com/@HotelUgly/videos"><img src="./imgg/history.png" alt="" width="60px" style="margin-right: 10px"></a>
+  <a href="https://www.youtube.com/@HotelUgly/videos"><img src="./assets/img/history.png" alt="" width="60px" style="margin-right: 10px"></a>
   
 </div>
 <div class="table-number">
